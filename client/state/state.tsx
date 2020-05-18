35fb3b7f33fdd912020f282bb5d393bdc6014f2f
shock <bill@greenling.com>
2020-05-17 23:25:56 -0500

    TOTAL Hack to set initial march cap state!

--- client/state/state.tsx
+++ client/state/state.tsx
@@ -2,9 +2,23 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { ApplicationState } from './state.types';
 import rootReducer from './reducers';
+import { UPDATE_NUM_ENTRY } from './actions/action_types';
 
 const initialState = {};
 
 const middleware = [thunk];
 
 export const store = createStore( rootReducer, initialState, applyMiddleware(...middleware));
+
+const marchCap = 1000;
+const payload = {
+  id: 'march_cap',
+  value: marchCap
+}
+
+setTimeout(() => {
+  store.dispatch({
+    type: UPDATE_NUM_ENTRY,
+    payload: payload
+  });
+}, 500);
