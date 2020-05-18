67c0f592dff9a7243cd57c616a4a0ccb05f0b792
shock <bill@greenling.com>
2020-05-18 11:53:12 -0500

    Correct way to initialize Redux state

--- client/state/state.tsx
+++ client/state/state.tsx
@@ -5,6 +5,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import Big from 'big.js';
 
 /*
   Internal Imports
@@ -14,40 +15,57 @@ import { ApplicationState } from './state.types';
 import rootReducer from './reducers';
 import { UPDATE_NUM_ENTRY } from './actions/action_types';
 
-const initialState = {};
+const marchCap = new Big(1000);
+const infantry = marchCap.div(3).round();
+const distance = marchCap.times(2).div(3).round();
+
+const initialState = {
+  field: {
+    value: {
+      'march_cap': marchCap,
+      'Infantry': infantry,
+      'Distance': distance
+    }
+  }
+};
 
 const middleware = [thunk];
 
+const composeEnhancers = composeWithDevTools({ trace: true, traceLimit: 25 });
 export const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(
+  composeEnhancers(
     applyMiddleware(...middleware),
   )
 );
 
-const marchCap = 1000;
-const payloads = [
-  {
-    id: 'march_cap',
-    value: marchCap
-  },
-  {
-    id: 'Infantry',
-    value: marchCap/2
-  },
-  {
-    id: 'Distance',
-    value: marchCap/2
-  },
-];
+/*
+  Example of Calling .dispatch directly on store
+*/
 
-setTimeout(() => {
-  payloads.map( (payload) => {
-    store.dispatch({
-      type: UPDATE_NUM_ENTRY,
-      payload: payload
-    });
-    return payload;
-  });
-}, 500);
+// const marchCap = 1000;
+// const payloads = [
+//   {
+//     id: 'march_cap',
+//     value: marchCap
+//   },
+//   {
+//     id: 'Infantry',
+//     value: marchCap/2
+//   },
+//   {
+//     id: 'Distance',
+//     value: marchCap/2
+//   },
+// ];
+
+// setTimeout(() => {
+//   payloads.map( (payload) => {
+//     store.dispatch({
+//       type: UPDATE_NUM_ENTRY,
+//       payload: payload
+//     });
+//     return payload;
+//   });
+// }, 500);
