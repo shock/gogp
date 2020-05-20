ede49d3003865052979a222b952ec29085a6da42
shock <bill@greenling.com>
2020-05-20 11:09:21 -0500

    save unsaved file

--- client/models/troop_def/__tests__/troop_def.ts
+++ client/models/troop_def/__tests__/troop_def.ts
@@ -1,23 +1,23 @@
-import { MTierDef } from '../';
+import { MTroopDef } from '../';
 import { Int, Percentage, TierNum, TroopType } from '../../form_calc.types';
 
-describe( 'MTierDef', () => {
+describe( 'MTroopDef', () => {
 
   describe('constructor', () => {
-    it('should return an instance of MTierDef', () => {
-      let instance = new MTierDef(TierNum.T12, 100 as Percentage, false);
-      expect(instance instanceof MTierDef).toEqual(true);
+    it('should return an instance of MTroopDef', () => {
+      let instance = new MTroopDef(TroopType.Infantry, 100 as Percentage, false);
+      expect(instance instanceof MTroopDef).toEqual(true);
     });
     it('should set the id property', () => {
-      let instance = new MTierDef(TierNum.T12, 100 as Percentage, false);
+      let instance = new MTroopDef(TierNum.T12, 100 as Percentage, false);
       expect(instance.tierNum).toEqual(TierNum.T12);
     });
     it('should set the marchCap property', () => {
-      let instance = new MTierDef(TierNum.T12, 100 as Percentage, false);
+      let instance = new MTroopDef(TierNum.T12, 100 as Percentage, false);
       expect(instance.percentage).toEqual(100 as Percentage);
     });
     it('should set the highestTier property', () => {
-      let instance = new MTierDef(TierNum.T12, 100 as Percentage, true);
+      let instance = new MTroopDef(TierNum.T12, 100 as Percentage, true);
       expect(instance.locked).toEqual(true);
     });
   });
